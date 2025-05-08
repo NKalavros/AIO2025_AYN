f031b2fdcd147143c0fabdfc2daf97f7323dabf9
Adam Walker <49929421+a-dev-walker@users.noreply.github.com>
2025-05-08 19:29:59 -0400

    Add sigprofiler processing files via upload

---
+++ scripts/SigProfiler/cesc_assignment.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH --job-name=aio_sigprofiler_assigner_cesc_sbs # Job name
+#SBATCH --mail-type=END,FAIL # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=adam.walker@nyulangone.org # Where to send mail
+#SBATCH --ntasks=1 # typically 1
+#SBATCH --cpus-per-task=5 #number of threads
+#SBATCH --mem=32gb # Job memory request
+#SBATCH --time=12:00:00 # Time limit hrs:min:sec
+#SBATCH --output=/gpfs/data/courses/aio2025/adw9882/Analysis/output/logs/aio_sigprofiler_assigner_cesc_sbs_%A_%a.log # Standard output and error log
+#SBATCH -p cpu_short #can be whatever size you feel you need
+
+## input data
+FILELOCATION="/gpfs/data/courses/aio2025/adw9882/Data"
+OUTPUTFILELOCATION="/gpfs/data/courses/aio2025/adw9882/Analysis/output"
+
+#Source Conda initialization script
+
+source /gpfs/data/courses/aio2025/adw9882/aio_analysis/etc/profile.d/conda.sh
+
+#Initialize conda
+
+eval "$(conda shell.bash hook)"
+
+#Activate your specific environment
+
+conda activate aio_py
+
+## Append proper name to make sure that you're using the right file
+CESCFILE="${FILELOCATION}/cesc_converted.txt"
+
+## output location and make a folder if it doesn't exist
+CESCOUTLOC="${OUTPUTFILELOCATION}/SBS_SigAssigner_output_cesc"
+
+mkdir -p ${CESCOUTLOC}
+
+## SigAssigner for Cosmic
+
+SigProfilerAssignment cosmic_fit \
+    ${CESCFILE} \
+    ${CESCOUTLOC} \
+    --context_type "SBS" \
+    --collapse_to_SBS96 "True" \
+    --verbose "True"
+    
+#Optionally deactivate when done
+
+conda deactivate
+
+# Print completion message
+#echo "SigProfiler assignment completed for project: ${INPUTFILE}"
